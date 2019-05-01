af11f831e18d0f19a337018d1c041963d22fb61c
Aervtas <klin0916@gmail.com>
2019-05-01 14:02:43 -0400

    Added process.env.PORT for heroku deployment

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ app.get('/',function(req,res){
   res.render('home',{});
 })
 
-app.listen(3000, function() {
-    console.log('Listening on port 3000!');
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening!');
 });
