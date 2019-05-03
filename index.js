029ad156b9f2712b17570370e2d2a423038b32b7
Aervtas <klin0916@gmail.com>
2019-05-03 11:13:37 -0400

    connected to mongoDB now

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var exphbs = require('express-handlebars');
+var mongoose = require('mongoose');
+var dotenv = require('dotenv');
+var Movie = require('./models/Movie');
 
 var app = express();
+
+// Load envirorment variables
+dotenv.config();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -16,6 +22,14 @@ app.use('/public', express.static('public'));
  * have '/api' prepended to them. Please remember that you need at least 5
  * endpoints for the API, and 5 others.
  */
+
+ // Connect to MongoDB
+ console.log(process.env.MONGODB)
+ mongoose.connect(process.env.MONGODB);
+ mongoose.connection.on('error', function() {
+     console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
+     process.exit(1);
+ });
 
 app.get('/',function(req,res){
   res.render('home',{});
