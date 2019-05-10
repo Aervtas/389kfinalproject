ed242ea78b29e4dd72a3a802f0a46de66c13d078
Atul Bharati <atul.bharati.2010@gmail.com>
2019-05-10 09:43:41 -0400

    updated files

--- index.js
+++ index.js
@@ -235,12 +235,125 @@ app.delete('/music/remove/:id', function(req, res){
   });
 });
 
+
+
+app.get('/game/tags', function(req, res){
+    res.render('gameTags');
+});
+
+app.get('/music/tags', function(req, res){
+    res.render('musicTags');
+});
+
+app.get('/game/greatest', function(req, res){
+    var tempID = 0;
+    var tempCheck = 0;
+    //loop through Game and find game with the most reviews
+    Game.find({}, function(err, games){
+      if (err) {
+        throw err;
+      }
+      for (var i = 0; i < games.length; i++) {
+        if(games[i].reviews.length >= tempCheck) {
+          tempID = games[i]._id;
+          tempCheck = games[i].reviews.length;
+        }
+      }
+      Game.findOne({_id: tempID}, function(err, game){
+        res.render('display', {
+          data: game
+        });
+      });
+    });
+});
+
+app.get('/music/greatest', function(req, res){
+    var tempID;
+    var tempCheck = 0;
+    //loop through Game and find music with the most reviews
+    Music.find({}, function(err, games){
+      if (err) {
+        throw err;
+      }
+      for (var i = 0; i < games.length; i++) {
+        if(games[i].reviews.length >= tempCheck) {
+          tempID = games[i]._id;
+          tempCheck = games[i].reviews.length;
+        }
+      }
+      Music.findOne({_id: tempID}, function(err, game){
+        res.render('display', {
+          data: game
+        });
+      });
+    });
+});
+
+app.get('/game/best', function(req, res){
+  var tempID = 0;
+  var tempCheck = 0;
+  //loop through Game and find game with the most reviews
+  Game.find({}, function(err, games){
+    if (err) {
+      throw err;
+    }
+    for (var i = 0; i < games.length; i++) {
+      var aver = 0;
+      for (var k = 0; k < games[i].reviews.length; k++) {
+        aver = aver + games[i].reviews[k].rating;
+      }
+      aver = aver / games[i].reviews.length;
+      if (aver >= tempCheck) {
+        tempID = games[i]._id;
+        tempCheck = aver;
+      }
+    }
+    Game.findOne({_id: tempID}, function(err, game){
+      res.render('display', {
+        data: game
+      });
+    });
+  });
+});
+
+app.get('/music/best', function(req, res){
+  var tempID = 0;
+  var tempCheck = 0;
+  //loop through Game and find game with the most reviews
+  Music.find({}, function(err, games){
+    if (err) {
+      throw err;
+    }
+    for (var i = 0; i < games.length; i++) {
+      var aver = 0;
+      for (var k = 0; k < games[i].reviews.length; k++) {
+        aver = aver + games[i].reviews[k].rating;
+      }
+      aver = aver / games[i].reviews.length;
+      if (aver >= tempCheck) {
+        tempID = games[i]._id;
+        tempCheck = aver;
+      }
+    }
+    Music.findOne({_id: tempID}, function(err, game){
+      res.render('display', {
+        data: game
+      });
+    });
+  });
+});
+
+app.get('/music/random', function(req, res){
+
+});
+
 app.get('/game/:id', function(req, res){
   Game.findOne({_id: req.params.id}, function(err, game){
     res.render('display', {
       data: game
     });
   });
+  res.render('notFound');
 });
 
 app.get('/music/:id', function(req, res){
@@ -249,14 +362,7 @@ app.get('/music/:id', function(req, res){
       data: music
     });
   });
-});
-
-app.get('/game/tags', function(req, res){
-  res.render('gameTags');
-});
-
-app.get('/music/tags', function(req, res){
-  res.render('musicTags');
+  res.render('notFound');
 });
 
 io.on('connection', function(socket) {
